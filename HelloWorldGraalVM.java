9c64e11f7a31706131cb97ea08f1f6d38f123262
swt02026 <swt02026@yahoo.com.tw>
2019-04-16 17:39:59 +0800

    Create HelloWorldGraalVM.java

---
+++ HelloWorldGraalVM.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import org.graalvm.polyglot.*;
+import org.graalvm.nativeimage.*;
+import java.util.*;
+
+import org.graalvm.nativeimage.c.CContext;
+import org.graalvm.nativeimage.c.constant.CConstant;
+import org.graalvm.nativeimage.c.constant.CEnum;
+import org.graalvm.nativeimage.c.constant.CEnumLookup;
+import org.graalvm.nativeimage.c.constant.CEnumValue;
+import org.graalvm.nativeimage.c.function.CEntryPoint;
+import org.graalvm.nativeimage.c.function.CEntryPointContext;
+import org.graalvm.nativeimage.c.function.CEntryPointLiteral;
+import org.graalvm.nativeimage.c.function.CLibrary;
+import org.graalvm.nativeimage.c.function.CFunction;
+import org.graalvm.nativeimage.c.function.CFunction.Transition;
+import org.graalvm.nativeimage.c.function.CFunctionPointer;
+import org.graalvm.nativeimage.c.function.InvokeCFunctionPointer;
+import org.graalvm.nativeimage.c.struct.AllowWideningCast;
+import org.graalvm.nativeimage.c.struct.CField;
+import org.graalvm.nativeimage.c.struct.CFieldAddress;
+import org.graalvm.nativeimage.c.struct.CFieldOffset;
+import org.graalvm.nativeimage.c.struct.CStruct;
+import org.graalvm.nativeimage.c.struct.SizeOf;
+import org.graalvm.nativeimage.c.type.CCharPointer;
+import org.graalvm.nativeimage.c.type.CIntPointer;
+import org.graalvm.nativeimage.c.type.CLongPointer;
+import org.graalvm.nativeimage.c.type.CTypeConversion;
+import org.graalvm.nativeimage.c.type.CTypeConversion.CCharPointerHolder;
+import org.graalvm.word.PointerBase;
+import org.graalvm.word.SignedWord;
+import org.graalvm.word.UnsignedWord;
+import org.graalvm.word.WordFactory;
+
+@CContext(Headers.class)
+@CLibrary("HelloWorldCallGO")
+public class HelloWorldGraalVM {
+
+    @CFunction(transition = Transition.NO_TRANSITION)
+    private native void heavyWork(int num);
+
+    public static void main(String[] args) {
+        final int num = Integer.valueOf(args[0]);
+        new HelloWorldGraalVM().heavyWork(num);
+
+    }
+}
